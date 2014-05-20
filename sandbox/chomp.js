6c08e7d386b6ff6cca7e5ddf6453a38e13b9ddfd
Ivan Pantic <ivanpantic82@gmail.com>
2014-05-20 10:29:50 +0200

    More tests, fixes

--- sandbox/chomp.js
+++ sandbox/chomp.js
@@ -1,5 +1,21 @@
+var Stream = require("stream");
+
 var libLineChomper = require("../lib/line-chomper");
 
-libLineChomper.chomp("../spec/files/small-nix.txt", function (err, lines) {
+/*libLineChomper.chomp("../spec/files/small-nix.txt", function (err, lines) {
+	console.log(err, lines);
+});*/
+
+var stream = new Stream.Readable();
+stream._read = function noop() {};
+
+stream.push("line1\n");
+stream.push("line2\n");
+stream.push("line3");
+
+libLineChomper.chomp(stream, function (err, lines) {
 	console.log(err, lines);
 });
+
+stream.resume();
+stream.push(null);
--- spec/line-chomper.spec.js
+++ spec/line-chomper.spec.js
@@ -1,36 +1,61 @@
+var Stream = require("stream");
+
 var libLineChomper = require("../lib/line-chomper");
 
 describe("Basic chomping", function () {
+
+	function verifySmallFileProcessedCorrectly(err, lines) {
+		expect(err).toBeNull();
+		expect(lines).not.toBeNull();
+		expect(lines.length).toEqual(5);
+		expect(lines[0]).toEqual("line1");
+		expect(lines[3]).toEqual("line4");
+		expect(lines[4]).toEqual("");
+	}
+
 	it("should correctly chomp lines with *nix-style terminators (\\r)", function (done) {
 		libLineChomper.chomp(__dirname + "/files/small-nix.txt", function (err, lines) {
-			expect(err).toBeNull();
-			expect(lines).not.toBeNull();
-			expect(lines.length).toEqual(5);
-			expect(lines[0]).toEqual("line1");
-			expect(lines[3]).toEqual("line4");
-			expect(lines[4]).toEqual("");
+			verifySmallFileProcessedCorrectly(err, lines);
 			done();
 		});
 	});
 	it("should correctly chomp lines with mac-style terminators (\\n)", function (done) {
 		libLineChomper.chomp(__dirname + "/files/small-mac.txt", function (err, lines) {
-			expect(err).toBeNull();
-			expect(lines).not.toBeNull();
-			expect(lines.length).toEqual(5);
-			expect(lines[0]).toEqual("line1");
-			expect(lines[3]).toEqual("line4");
-			expect(lines[4]).toEqual("");
+			verifySmallFileProcessedCorrectly(err, lines);
 			done();
 		});
 	});
 	it("should correctly chomp lines with PC-style terminators (\\r\\n)", function (done) {
 		libLineChomper.chomp(__dirname + "/files/small-pc.txt", function (err, lines) {
+			verifySmallFileProcessedCorrectly(err, lines);
+			done();
+		});
+	});
+
+	it("should accept custom stream", function (done) {
+		var stream = new Stream.Readable();
+		stream._read = function noop() {};
+
+		stream.push("line1\n");
+		stream.push("line2");
+
+		libLineChomper.chomp(stream, function (err, lines) {
 			expect(err).toBeNull();
 			expect(lines).not.toBeNull();
-			expect(lines.length).toEqual(5);
+			expect(lines.length).toEqual(2);
 			expect(lines[0]).toEqual("line1");
-			expect(lines[3]).toEqual("line4");
-			expect(lines[4]).toEqual("");
+			expect(lines[1]).toEqual("line2");
+			done();
+		});
+
+		stream.resume();
+		stream.push(null);
+	});
+
+	it("should return error for missing files", function (done) {
+		libLineChomper.chomp(__dirname + "/files/not-there.txt", function (err, lines) {
+			expect(err).not.toBeNull();
+			expect(err.code).toEqual("ENOENT");
 			done();
 		});
 	});
