cb65f0698bf5555ed9070abc64e979f0ca4437c9
Ivan Pantic <ivanpantic82@gmail.com>
2014-05-20 11:21:05 +0200

    Advanced chomping tests

--- spec/line-chomper.spec.js
+++ spec/line-chomper.spec.js
@@ -1,6 +1,7 @@
 var Stream = require("stream");
 
-var libLineChomper = require("../lib/line-chomper");
+var libTools = require("../lib/tools"),
+	libLineChomper = require("../lib/line-chomper");
 
 describe("Basic chomping", function () {
 
@@ -74,6 +75,65 @@ describe("Basic chomping", function () {
 		libLineChomper.chomp(__dirname + "/files/not-there.txt", function (err, lines) {
 			expect(err).not.toBeNull();
 			expect(err.code).toEqual("ENOENT");
+			done();
+		});
+	});
+});
+
+describe("Advanced chomping", function () {
+	beforeEach(function () {
+		this.addMatchers({
+			toBeANumber: function () {
+				return libTools.isNumber(this.actual);
+			}
+		})
+	});
+
+	it("can chomp file one line at a time, without buffering", function (done) {
+		var counter = 0;
+		libLineChomper.chomp(
+			__dirname + "/files/small-nix.txt",
+			function (line, offset, sizeInBytes) {
+				expect(offset).toBeANumber();
+				expect(sizeInBytes).toBeANumber();
+				counter++;
+			},
+			function (err, lines) {
+				expect(err).toBeNull();
+				expect(lines).toBeFalsy();
+				expect(counter).toEqual(4);
+				done();
+			}
+		);
+	});
+
+	function verifySmallFileByteCounts(lines, singleCharTerminator) {
+		if (singleCharTerminator) {
+			expect(lines[3].offset).toEqual(13);
+			expect(lines[3].sizeInBytes).toEqual(6);
+		} else {
+			expect(lines[3].offset).toEqual(16);
+			expect(lines[3].sizeInBytes).toEqual(7);
+		}
+	}
+
+	it("can correctly count bytes in file with *nix-styled line terminators", function (done) {
+		libLineChomper.chomp(__dirname + "/files/small-nix.txt", { returnDetails: true }, function (err, lines) {
+			verifySmallFileByteCounts(lines, true);
+			done();
+		});
+	});
+
+	it("can correctly count bytes in file with mac-styled line terminators", function (done) {
+		libLineChomper.chomp(__dirname + "/files/small-mac.txt", { returnDetails: true }, function (err, lines) {
+			verifySmallFileByteCounts(lines, true);
+			done();
+		});
+	});
+
+	it("can correctly count bytes in file with pc-styled line terminators", function (done) {
+		libLineChomper.chomp(__dirname + "/files/small-pc.txt", { returnDetails: true }, function (err, lines) {
+			verifySmallFileByteCounts(lines, false);
 			done();
 		});
 	});
